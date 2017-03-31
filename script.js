d397d83252c3c0367f710adca3af5730fc1fa2ed
ctazuke <ctazuke@id.iit.edu>
2017-03-31 12:08:58 -0500

    Moved the first box to left and down in script.js

--- script.js
+++ script.js
@@ -6,8 +6,22 @@ boxes[2].style.backgroundColor = "yellow";
 console.log(boxes[0]);
 console.log(boxes[2]);
 
+var leftMargin = 0;
+var topMargin = 0;
+var moveAmount = 20;
+
 function boxClicked() {
   console.log("Hey, box clicked!");
+
+  if (leftMargin < 100) {
+    leftMargin += moveAmount;
+  } else {
+    leftMargin -= 100;
+  }
+
+  topMargin += moveAmount;
+  boxes[0].style.marginLeft = leftMargin + "px";
+  boxes[0].style.marginTop = topMargin + "px";
 }
 
 boxes[0].addEventListener("click", boxClicked);
