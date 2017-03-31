f8604cbf32c9a8e31a24f8a385f34b39199a2cf7
ctazuke <ctazuke@id.iit.edu>
2017-03-31 11:26:51 -0500

    grab all .box elements and change bo 1 to blue and bo 3 to yellow.

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert("Hello.");
+var boxes = document.querySelectorAll(".box");
+
+boxes[0].style.backgroundColor = "blue";
+boxes[2].style.backgroundColor = "yellow";
+
+console.log(boxes[0]);
+console.log(boxes[2]);
